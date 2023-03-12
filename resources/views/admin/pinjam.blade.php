a1a875afe0e484525572a62325face3c592eb554
Ardi1233 <3103120029@student.smktelkom-pwt.sch.id>
2023-03-12 09:38:29 +0700

    first commit

---
+++ resources/views/admin/pinjam.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+@section('title', 'Ardia | Halaman Pinjam')
+@section('content')
+<!-- PAGE CONTENT-->
+<div class="page-content--bgf7">
+    <!-- DATA TABLE-->
+    <section class="p-t-20">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="title-1 m-b-20"><strong>Selamat anda berhasil meminjam buku Silahkan ambil buku di kasir perpustakaan<strong></h3>
+                    <div class="table-data__tool">
